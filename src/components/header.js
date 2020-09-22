147bde6dd409c50bd894e4273bd6cc09273581ad
Chris Carty <chriscarty@Chriss-MacBook-Pro-2.local>
2020-09-22 16:44:37 +0100

    Update 2

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import logo from "../images/logo-designcode.svg"
-import '/Header.css'
+import './Header.css'
 
 
 class Header extends React.Component {
